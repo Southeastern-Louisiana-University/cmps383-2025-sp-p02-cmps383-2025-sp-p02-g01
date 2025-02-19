3f3f88abc6df9df11bfad98f531f8ae58a2465a8
Nobody1989 <carsonshasteen1989@gmail.com>
2025-02-19 17:24:19 -0600

    user role and user role
    set the entities in the folder

---
+++ Selu383.SP25.P02.Api/Features/Roles/Role.cs
@@ -0,0 +1,16 @@
+﻿namespace Selu383.SP25.P02.Api.Features.Theaters.Roles
+{
+    public class Role
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        // Navigation property for many-to-many relationship with User
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+
+        public Role()
+        {
+            UserRoles = new HashSet<UserRole>();
+        }
+    }
+}
---
+++ Selu383.SP25.P02.Api/Features/Users/User.cs
@@ -0,0 +1,16 @@
+﻿namespace Selu383.SP25.P02.Api.Features.Users
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        // Navigation property for many-to-many relationship with Role
+        public virtual ICollection<UserRole> UserRoles { get; set; }
+
+        public User()
+        {
+            UserRoles = new HashSet<UserRole>();
+        }
+    }
+}
