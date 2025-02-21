dc04e2120c07684e34819c12ad0c627268bbcb08
Aakash Poudel <aakash.poudel.900@gmail.com>
2025-02-20 18:40:27 -0600

    data seeded, Program.cs optimized for seed

--- Selu383.SP25.P02.Api/Data/DataContext.cs
+++ Selu383.SP25.P02.Api/Data/DataContext.cs
@@ -1,12 +1,40 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using Selu383.SP25.P02.Api.Features.Theaters;
+using Selu383.SP25.P02.Api.Features.Users;
+using Selu383.SP25.P02.Api.Features.Theaters.Roles;
+using Selu383.SP25.P02.Api.Features.UserRoles;
+using Microsoft.AspNetCore.Identity;
 
 namespace Selu383.SP25.P02.Api.Data
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<User, Role, int,
+        IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public DataContext(DbContextOptions<DataContext> options) : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            // Configure UserRole relationship
+            builder.Entity<UserRole>(userRole =>
+            {
+                userRole.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+                userRole.HasOne(ur => ur.Role)
+                    .WithMany(r => r.Users)
+                    .HasForeignKey(ur => ur.RoleId)
+                    .IsRequired();
+
+                userRole.HasOne(ur => ur.User)
+                    .WithMany(r => r.Roles)
+                    .HasForeignKey(ur => ur.UserId)
+                    .IsRequired();
+            });
         }
 
         public DbSet<Theater> Theaters { get; set; }
--- Selu383.SP25.P02.Api/Program.cs
+++ Selu383.SP25.P02.Api/Program.cs
@@ -1,8 +1,10 @@
-
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Selu383.SP25.P02.Api.Data;
+// New Identity-related using statements
+using Selu383.SP25.P02.Api.Features.Users;
+using Selu383.SP25.P02.Api.Features.Theaters.Roles;
 
 namespace Selu383.SP25.P02.Api
 {
@@ -11,15 +13,17 @@ namespace Selu383.SP25.P02.Api
         public static async Task Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
             // Add services to the container.
             builder.Services.AddDbContext<DataContext>(options =>
                 options.UseSqlServer(builder.Configuration.GetConnectionString("DataContext") ?? throw new InvalidOperationException("Connection string 'DataContext' not found.")));
 
+            //  Adds Identity configuration before other services
+            builder.Services.AddIdentity<User, Role>()
+                .AddEntityFrameworkStores<DataContext>()
+                .AddDefaultTokenProviders();
+
             builder.Services.AddControllers();
             // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-
-
             builder.Services.Configure<IdentityOptions>(options =>
             {
                 // Password 
@@ -29,45 +33,39 @@ namespace Selu383.SP25.P02.Api
                 options.Password.RequireUppercase = true;
                 options.Password.RequiredLength = 6;
                 options.Password.RequiredUniqueChars = 1;
-
                 // Lockout 
                 options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
                 options.Lockout.MaxFailedAccessAttempts = 5;
                 options.Lockout.AllowedForNewUsers = true;
-
                 // User 
                 options.User.AllowedUserNameCharacters =
                 "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
                 options.User.RequireUniqueEmail = false;
             });
-
             builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-    .AddCookie();
+                .AddCookie();
             builder.Services.ConfigureApplicationCookie(options =>
             {
                 // Cookie 
                 options.Cookie.HttpOnly = true;
                 options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
-
                 options.LoginPath = "/Identity/Account/Login";
                 options.AccessDeniedPath = "/Identity/Account/AccessDenied";
                 options.SlidingExpiration = true;
             });
-
-
-
-
-
-
-
             builder.Services.AddOpenApi();
-
             var app = builder.Build();
 
+            // Modified database initialization and seeding
             using (var scope = app.Services.CreateScope())
             {
                 var db = scope.ServiceProvider.GetRequiredService<DataContext>();
                 await db.Database.MigrateAsync();
+
+                // Seeds users and roles first
+                await SeedUsers.Initialize(scope.ServiceProvider);
+
+                //Seeds theaters
                 SeedTheaters.Initialize(scope.ServiceProvider);
             }
 
@@ -78,15 +76,11 @@ namespace Selu383.SP25.P02.Api
             }
 
             app.UseHttpsRedirection();
-
             app.UseAuthentication();
             app.UseAuthorization();
-            
 
-          
             app.MapControllers();
             app.UseStaticFiles();
-
             if (app.Environment.IsDevelopment())
             {
                 app.UseSpa(x =>
@@ -98,9 +92,7 @@ namespace Selu383.SP25.P02.Api
             {
                 app.MapFallbackToFile("/index.html");
             }
-
             app.Run();
-
         }
     }
 }
